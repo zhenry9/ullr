45b6ca5e6eb306856ff74bd9ca805e136fdd6544
zhenry9 <zhenry9@gmail.com>
2021-02-28 01:00:12 -0800

    Set for new version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-# from dweet2ser import __version__ as version
+from dweet2ser import __version__ as version
 
 # except ImportError:
 #    from distutils.core import setup
@@ -10,14 +10,14 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='dweet2ser',
-    version="0.2.0a",
+    version=version,
     author='Zach Henry',
     author_email='zhenry9@gmail.com',
     description='A dweet.io <-> serial interface',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/zhenry9/dweet2ser',
-    download_url=f'https://github.com/zhenry9/dweet2ser/archive/0.2.0a.tar.gz',
+    download_url=f'https://github.com/zhenry9/dweet2ser/archive/{version}.tar.gz',
     packages=setuptools.find_packages(exclude=('tests',)),
     include_package_data=True,
     classifiers=[
