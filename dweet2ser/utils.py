e4f4e2865c7ff245af5da614152483d93cc676ee
Zach <51764547+zhenry9@users.noreply.github.com>
2021-03-06 16:57:31 -0800

    Got port detection working, improved webui

--- dweet2ser/utils.py
+++ dweet2ser/utils.py
@@ -2,6 +2,9 @@ import datetime
 import os
 import socket
 import re
+import sys
+import glob
+import serial
 import logging
 import logging.handlers
 
@@ -96,12 +99,32 @@ def get_ip():
     return IP
 
 def get_available_com_ports():
-    names = []
-    import serial.tools.list_ports
-    ports = list(serial.tools.list_ports.comports())
-    for p in ports:
-        names.append(p[0])
-    return names
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(100)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
 
 def print_to_ui(message, endline="\n", sys=False):
     ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
