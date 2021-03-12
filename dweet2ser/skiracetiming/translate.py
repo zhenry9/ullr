b4789f962ea627b7f66aaa5c83ddfe8119886b23
Zach Henry <zhenry9@gmail.com>
2021-03-12 10:45:32 -0800

    Formatting, new version

--- dweet2ser/skiracetiming/translate.py
+++ dweet2ser/skiracetiming/translate.py
@@ -1,13 +1,14 @@
 # All encode functions take a channel and tod (time of day) argument. The channel argument is a list.
 # The first item on the list is an integer representing the actual channel number. The channel number should
 # be normalized, so the first channel is 0, whether or not the timer itself starts its channel counts at 0.
-# The second item in the channel list argument is a boolean value indicating whether or not the impulse was 
-# manual, eg. from a pushbutton on the timer. So, a channel argument of [0, True] would compute to "c0M" on 
-# an Alge Timy, because Timy channels start at 0. It would compute to "M1" on a Tag CP540 because CP540 
+# The second item in the channel list argument is a boolean value indicating whether or not the impulse was
+# manual, eg. from a pushbutton on the timer. So, a channel argument of [0, True] would compute to "c0M" on
+# an Alge Timy, because Timy channels start at 0. It would compute to "M1" on a Tag CP540 because CP540
 # channels start at 1, and "SZ " on an s4 because s4s are weird. The same logic applies to decoding.
 
 class TranslationError(Exception):
     pass
+
 
 def _decode_cp540(message):
     chan = message[13:15]
@@ -15,8 +16,10 @@ def _decode_cp540(message):
     channel = ['', False]
     if chan[0].upper() == "M":
         channel[1] = True
-    channel[0] = int(chan[1]) - 1  # First channel of CP540 is 1. This normalizes to first channel of 0
+    # First channel of CP540 is 1. This normalizes to first channel of 0
+    channel[0] = int(chan[1]) - 1
     return channel, tod
+
 
 def _encode_cp540(channel, tod):
     channel[0] += 1  # First channel of CP540 is 1, not 0.
@@ -29,9 +32,11 @@ def _encode_cp540(channel, tod):
     else:
         chan = f" {channel[0]}"
     msg = f"TN           {chan} {tod}      "
-    cs16 = f'{int(sum(bytearray(msg.encode("ascii"))) % 65536):04x}'.upper() #calculate checksum
+    # calculate checksum
+    cs16 = f'{int(sum(bytearray(msg.encode("ascii"))) % 65536):04x}'.upper()
     msg += f"\t{cs16}\r\n"
     return msg
+
 
 def _decode_timy(message):
     chan = message[6:9]
@@ -42,6 +47,7 @@ def _decode_timy(message):
     channel[0] = int(chan[1])
     return channel, tod
 
+
 def _encode_timy(channel, tod):
     if channel[0] > 7 or channel[0] < 0:
         raise TranslationError(f"Channel {channel[0]} out of range for Timy.")
@@ -50,7 +56,8 @@ def _encode_timy(channel, tod):
     else:
         chan = f"c{channel[0]} "
     msg = f"      {chan} {tod}       \r"
-    return msg 
+    return msg
+
 
 def _decode_s4(message):
     chan = message[5:8]
@@ -61,6 +68,7 @@ def _decode_s4(message):
     else:
         channel[0] = int(chan[1:])
     return channel, tod
+
 
 def _encode_s4(channel, tod):
     if channel[0] > 17 or channel[0] < 0:
@@ -84,7 +92,9 @@ def _encode_s4(channel, tod):
 #       O = Origin, "M" for manual or physical input, "R" for radio
 #       P = Sign, 1 for negative, space otherwise
 #       T = Time with no ":", HHMMSSttt
-# Example: "000100010012552550M 123456789\r" Finish at 12:34:56.789, either manually or from cable 
+# Example: "000100010012552550M 123456789\r" Finish at 12:34:56.789, either manually or from cable
+
+
 def _decode_racetime2(message):
     chan = int(message[14:17])
     channel = ["", False]
@@ -101,10 +111,12 @@ def _decode_racetime2(message):
     tod = f"{message[20:22]}:{message[22:24]}:{message[24:26]}.{message[26:29]}"
     return channel, tod
 
+
 def _encode_racetime2(channel, tod):
     # try to map normalized channel numbers to the physical channels of racetime2
-    if channel [0] > 255 or channel[0] < 0:
-        raise TranslationError(f"Channel {channel[0]} out of range for RaceTime2.")
+    if channel[0] > 255 or channel[0] < 0:
+        raise TranslationError(
+            f"Channel {channel[0]} out of range for RaceTime2.")
     if channel[0] == 0:
         chan = "000"
     elif channel[0] == 1:
@@ -113,9 +125,11 @@ def _encode_racetime2(channel, tod):
         chan = 254
     else:
         chan = f"{(channel[0]-2):03d}"
-    tod = tod[:12].replace(":", "").replace(".", "")  # remove last digit and punctuation
+    tod = tod[:12].replace(":", "").replace(
+        ".", "")  # remove last digit and punctuation
     msg = f"00010001001{chan}{chan}0M {tod}\r"
     return msg
+
 
 ENCODE = {
     "cp540": _encode_cp540,
@@ -130,6 +144,7 @@ DECODE = {
     "s4": _decode_s4,
     "racetime2": _decode_racetime2
 }
+
 
 def translate(message, source, destination, shift=0):
     channel, tod = DECODE[source](message)
