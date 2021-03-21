98052c8bd60547e38562cbab1363274157cb7fef
Zach <51764547+zhenry9@users.noreply.github.com>
2021-03-21 12:29:44 -0700

    Packaged for windows with briefcase

--- dweet2ser/webapp/__init__.py
+++ dweet2ser/webapp/__init__.py
@@ -1,13 +1,21 @@
 import flask
 import flask_socketio
 import logging
+import webbrowser
 
+PORT = 5000
 webapp = flask.Flask(__name__)
 webapp.app_context().push()
 socketio = flask_socketio.SocketIO(webapp)
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+def port_in_use(port):
+    import socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', port)) == 0
 
 def run():
-    socketio.run(webapp, host="0.0.0.0")
+    webbrowser.open(f"http://localhost:{PORT}", new=1)
+    if not port_in_use(PORT):
+        socketio.run(webapp, host="0.0.0.0")
